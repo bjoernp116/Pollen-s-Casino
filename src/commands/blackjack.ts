2ab09cb5b952c98d9f76a9ece44d0d67942b1314
Bjørn Pollen <bjoernpollen@gmail.com>
2024-02-15 23:48:18 +0100

    Blackjack

--- src/commands/blackjack.ts
+++ src/commands/blackjack.ts
@@ -2,28 +2,49 @@ import { CommandInteraction, RoleResolvable, SlashCommandBuilder } from "discord
 import { getMoney } from "../data";
 
 export const data = new SlashCommandBuilder()
-    .setName("balance")
-    .setDescription("Check out your money!")
-    .addUserOption(option=>
+    .setName("blackjack")
+    .setDescription("Get as close to 21 as possible! (whitout going over)")
+    .addIntegerOption(option=>
         option
-            .setName("target")
-            .setDescription("Look at other peoples money")
-            .setRequired(false));
+            .setName("bet")
+            .setDescription("How much Are you Wagaring?")
+            .setRequired(true));
+interface Card{
+    name: string;
+    value: number;
+}
+const DECK = [":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":keycap_ten:", ":prince:", ":princess:", ":crown:", ":a:"];
+const VALUE = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
+function drawcard():Card{
+    let i = Math.floor(Math.random()*13);
+    return {name:DECK[i], value: VALUE[i]};
+}
+function calchand(cards: Array<Card>): [number, boolean]{
+    let sum: number = 0;
+    let soft = false;
+    for(let card of cards as Card[]){
+        sum += card.value;
+        if(card.name == "A")
+            soft=true;
+    }
+    return [sum, soft];
+}
 export async function execute(interaction: any) {
-    let target = interaction.options.getUser("target") ?? false;
-    let msg = "You have ";
-    if(target!=false){
-        interaction.reply(target.tag+" has **"+getMoney(target.tag)+"$**");
-    }
-    if(!interaction.member?.roles.cache.has("1207714834041413693") && getMoney(interaction.user.tag)>1000){
-        msg = "Congratulations, you are officially addicted to this game! You hve ";
-        interaction.member.roles.add("1207714834041413693");
-    
-    }
-    if(interaction.member?.roles.cache.has("1207714834041413693") && getMoney(interaction.user.tag)<1000){
-        msg = "You sadly lost you role as addicted :(. You have mearly ";
-        interaction.member.roles.remove("1207714834041413693");
-    }
-    return interaction.reply(msg+getMoney(interaction.user.tag)+"$!");
+    let D: Array<Card> = [];
+    let Y: Array<Card> = [];
+    D.push(drawcard());
+    Y.push(drawcard());
+    Y.push(drawcard());
+    let [Dv, Ds] = calchand(D);
+    let [Yv, Ys] = calchand(Y);
+    let embed = {
+        title: "Blackjack!",
+        description: "Hit on 16, stand on 17.",
+        fields: [
+            {name:"Dealer:", value: `${D.map(x=>x.name).join("")} (${Dv})${Ds?"/":""}`},
+            {name:"You:", value: `${Y.map(x=>x.name).join("")} (${Yv})${Ys?"/":""}`}
+        ]
+    };
+    interaction.channel.send({embeds: [embed]});
 
 }
